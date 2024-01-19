0a53fdeb350cb0ae85283f40cc3bbbad261e02e6
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-01-19 16:02:47 +0100

    unique toc identifier, apply .css

--- R/knitXwp.R
+++ R/knitXwp.R
@@ -5,18 +5,58 @@ library("markdown")
 options(WordpressLogin = c(c(username='password')),
         WordpressURL = 'https://example.com/xmlrpc.php')
 
-knit_xwp<-function (input, title = "A post from R", ...,
-          action = c("newPost", "editPost", "newPage"),
-          postid, publish = TRUE)
-{
-  rmd<-input
-  render(rmd)
-  md.ns<-gsub("\\.Rmd",".md",rmd)
-  p.content<-readLines(md.ns)
-  p.content
-  p.html<-mark(p.content)
-  content<-p.html
 
+knit_xwp<-function (input, apply.css=F, title = "A post from R", ...,
+          action = c("newPost", "editPost", "newPage"),
+          postid = 0, publish = TRUE)
+{
+
+  get.pid<-function(postid){
+    ns.rnd<-c(sample(letters,26),sample(0:9,9))
+    ifelse(postid>0,pid<-postid,
+           pid<-paste0(sample(ns.rnd,6),collapse = ""))
+
+  }
+
+  get.css<-function(pid,template,p.html){
+    css<-paste0(readLines("style.css"),collapse ="")
+    css<-paste0("#knitxwp-",pid," ",css,collapse = "")
+    css<-paste0("<style>",css,"</style>",collapse = "")
+    h.content<-paste0(css,'<div id="knitxwp-',pid,'">',p.html,'</div>',collapse = "")
+  }
+  get.toc.unique<-function(pid,p.content){
+    m1<-grep("\\{#",p.content)
+    if(sum(m1)>0)
+      toc.cl<-gsub("\\(#","(#_pid_-",p.content[m1])
+    toc.cl<-gsub("\\{#","{#_pid_-",toc.cl)
+    toc.cl<-gsub("_pid_",pid,toc.cl)
+    toc.cl<-gsub("_pid_",pid,toc.cl)
+    m2<-grep("\\{#.+?-toc-",toc.cl)
+    if(sum(m2)>0)
+      toc.cl[m2]<-gsub("\\{#.+}","",toc.cl[m2])
+    toc.cl<-gsub("_pid_",pid,toc.cl)
+    p.content[m1]<-toc.cl
+    return(p.content)
+  }
+
+  rmd<-input
+  ext<-gsub(".*((\\.)(.*))","\\3",rmd)
+  f.ns<-gsub("(.*)((\\.)(.*))","\\1",rmd)
+  if(ext=="Rmd"){
+    render(rmd,output_dir = tempdir())
+  #md.ns<-gsub("\\.Rmd",".md",rmd)
+  md.ns<-paste0(tempdir(),"/",f.ns,".md")
+  p.content<-readLines(md.ns)
+  }
+  if(ext=="md")
+    p.md<-readLines(rmd)
+  pid<-get.pid(postid)
+  p.md<-get.toc.unique(pid,p.content)
+  p.html<-mark(p.md)
+  content<-p.html
+  if(apply.css==T)
+     content<-get.css(pid,"style.css",p.html)
+  writeLines(content,"output.html")
   #on.exit(unlink(out))
 #  content = file_string(out)
   #content = mark(text = out)
@@ -33,7 +73,7 @@ knit_xwp<-function (input, title = "A post from R", ...,
   title = enc2utf8(title)
   action = match.arg(action)
   WPargs = list(content = list(description = content, title = title,
-                               ...), appID = "RStudio", publish = publish)
+                               ...), publish = publish)
   if (action == "editPost")
     WPargs = c(postid = postid, WPargs)
   do.call(action, args = WPargs)
---
+++ test/notes.R
@@ -0,0 +1,54 @@
+input<-"wp001.Rmd"
+input<-rmd
+input<-"wp002.md"
+apply.css<-T
+postid<-2095
+title<-"testrwp"
+style<-"<style>pre{width: 300px;overflow: scroll;}</style>"
+
+tempmd<-tempdir("mddir")
+pid<-get.pid()
+ifelse(exists("postid"),pid<-postid,
+       ns.rnd<-c(sample(letters,26),sample(0:9,9)),
+       pid<-paste0(sample(ns.rnd,6),collapse = ""))
+
+get.css<-function(postid,template,p.html){
+css<-paste0(readLines("style.css"),collapse ="")
+css<-paste0("#knitxwp-",postid," ",css,collapse = "")
+css<-paste0("<style>",css,"</style>",collapse = "")
+css
+h.content<-paste0(css,'<div id="knitxwp-',postid,'">',p.html,'</div>',collapse = "")
+#writeLines(h.content,"output.html")
+}
+content<-get.css(2095,"style.css",p.html)
+rem.toc.obs<-function(p.content){
+  content.cl<-gsub("(\\{#toc-(.+?)\\})","",p.content)
+}
+content.cl
+rm(get.css)
+rm(rem.toc.obs)
+
+### unique id for toc entries necessary, else a click might jump to a similar entry on another post
+p.md
+postid<-0
+p.content
+pid<-get.pid(0)
+get.toc.unique<-function(pid,p.content){
+  m<-grep("\\{#",p.content)
+  p.content[m]
+  if(sum(m)>0)
+    toc.cl<-gsub("\\(#","(#_pid_-",p.content[m])
+    toc.cl<-gsub("\\{#","{#_pid_-",toc.cl)
+  toc.cl<-gsub("_pid_",pid,toc.cl)
+  toc.cl<-gsub("_pid_",pid,toc.cl)
+  m<-grep("\\{#.+?-toc-",toc.cl)
+  if(sum(m)>0)
+    toc.cl[m]<-gsub("\\{#.+}","",toc.cl[m])
+  #toc.id
+  toc.cl<-gsub("_pid_",pid,toc.cl)
+  toc.cl
+  [m]<-toc.id
+  }
+p.un<-get.toc.unique(0,p.content)
+p.un
+
